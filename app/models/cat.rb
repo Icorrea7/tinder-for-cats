075da0de6261f0ab3a013d7313072fd2c2c55b23
Isabel Correa <isacorrea07@gmail.com>
2022-11-30 07:40:04 -0500

    feat: add model validations

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,8 @@
 class Cat < ApplicationRecord
+  before_save { self.email = email.downcase }
+
+  enum status: [:single, :open_relationship, :just_friendship]
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
 end
