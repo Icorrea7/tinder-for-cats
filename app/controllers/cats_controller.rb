c203bd992815cc44190b57aae87c998bdb49151a
Isabel Correa <isacorrea07@gmail.com>
2022-11-30 08:23:41 -0500

    feat: add controller and routes

---
+++ app/controllers/cats_controller.rb
@@ -0,0 +1,19 @@
+class CatsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  private
+  def cats_params
+    params.permit(
+      :id,
+      :name,
+      :last_name,
+      :email,
+      :phone,
+      :status
+    )
+  end
+end
